fbd31c99d1885a3623f8f389812b5bb2e79ea9c7
Anikesh Sahai <anikeshsahai.shopify878@webkul.in>
2024-05-01 18:11:43 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./Pages/Product";
+import Homepage from "./Pages/Homepage";
+import Pricing from "./Pages/Pricing";
+import PageNotFound from "./Pages/PageNotFound";
+import AppLayout from "./Pages/AppLayout";
+import Login from "./Pages/Login";
+
+
 function App() {
-  return <div>App</div>;
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="product" element={<Product />} />
+      <Route path="pricing" element={<Pricing />} />
+      <Route path="app" element={<AppLayout />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App;
---
+++ src/Pages/AppLayout.jsx
@@ -0,0 +1,10 @@
+import AppNav from "../Components/AppNav";
+
+function AppLayout() {
+    return <div>
+        <AppNav />
+        <h1>AppLayout</h1>
+    </div>;
+}
+
+export default AppLayout;
