a38a919f35f876877ec3e3e126ac89ac89fb1fa6
Anikesh Sahai <anikeshsahai.shopify878@webkul.in>
2024-05-01 15:10:30 +0530

    wroldWise new rect app to unterstand routes

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
+
+
+
+// dev dependices for eslint
+
+//npm i eslint vite-plugin-eslint eslint-config-react-app --save-dev
